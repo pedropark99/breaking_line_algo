601662d6167f047a3c3e07115f655ae6c98c9ad6
pedropark99 <pedropark99@gmail.com>
2023-09-20 21:19:20 -0300

    Calculating the total width

--- python/main.py
+++ python/main.py
@@ -66,23 +66,20 @@ def glue(width,
         type = ItemType(1),
         char = " ",
         width = width,
-        shrink_factor = shrink_factor,
-        sthretch_factor = sthretch_factor
+        shrink_factor = width * shrink_factor,
+        sthretch_factor = width * sthretch_factor
     )
 
 
 
-def block(char,
-          width,
-          shrink_factor = SHRINK_FACTOR,
-          sthretch_factor = STHRETCH_FACTOR):
+def block(char, width):
 
     return ParagraphItem(
         type = ItemType(2),
         char = char,
         width = width,
-        shrink_factor = shrink_factor,
-        sthretch_factor = sthretch_factor
+        shrink_factor = 0,
+        sthretch_factor = 0
     )
 
 
@@ -90,7 +87,7 @@ def penalty(penalty_factor, flag, width = 0.0):
 
     return ParagraphItem(
         type = ItemType(3),
-        char = "",
+        char = None,
         width = width,
         shrink_factor = 0.0,
         sthretch_factor = 0.0,
@@ -98,8 +95,14 @@ def penalty(penalty_factor, flag, width = 0.0):
         flag = flag
     )
 
+def is_penalty_item(item):
+    return item.type == ItemType(3)
 
+def is_block_item(item):
+    return item.type == ItemType(2)
 
+def is_glue_item(item):
+    return item.type == ItemType(1)
 
 symbols = set([
     '.', '!', '$', ',',
@@ -113,16 +116,54 @@ symbols = set([
 
 chars = glyph_positions["character"]
 glyph_widths = glyph_positions["x_advance"]
-paragrah_items = list()
+paragraph_items = list()
 for index in range(n_rows - 1):
     current_width = glyph_widths[index]
     current_char = chars[index]
+    next_char = chars[index + 1]
+
+    if current_char == " ":
+        paragraph_items.append(glue(current_width))
+        continue
+    
+    elif current_char != " " and next_char == " ":
+        paragraph_items.append(penalty(0.0, 0, 0.0))
+        continue
+
+    else:
+        paragraph_items.append(block(current_char, current_width))
+        continue
+
+# Including the final penalty item in the paragraph which needs to
+# be a forced breakpoint
+paragraph_items.append(penalty(-1000, 0, 0.0))
 
 
 
+def get_total_width_at_range(start_index, end_index):
+    items = paragraph_items[start_index:end_index]
+    widths, stretchs, shrinks = list(), list(), list()
+    for item in items:
+        widths.append(item.width)
+        stretchs.append(item.sthretch_factor)
+        shrinks.append(item.shrink_factor)
+
+    return {
+        'total_width': sum(widths),
+        'total_stretch': sum(stretchs),
+        'total_shrink': sum(shrinks),
+    }
+
+
+start_index_of_current_line = 0
+for index in range(len(paragraph_items)):
+    current_item = paragraph_items[index]
+    if is_penalty_item(current_item):
+        current_line_width = get_total_width_at_range(
+            start_index_of_current_line,
+            index
+        )
 
 
 
-# for block in blocks:
-#     block.print()
-
+[item.print() for item in paragraph_items]
